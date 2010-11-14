4e32bfba39bfe81939b1075c9e468569f997a43f
Deb Bassett <deb@urbanwide.com>
2010-11-14 23:09:07 +0000

    fix file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :deploy_to, "/var/www/#{application}"
 
 desc "Copy one of the sample database.yml files and update image locations"
-task :after_update_code, :roles => :app do*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
+task :after_update_code, :roles => :app do
   run "cp #{release_path}/config/database.live.yml #{release_path}/config/database.yml"
 end
